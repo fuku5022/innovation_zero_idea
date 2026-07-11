7b551d22896774761bfca3719e0541ae874306d4
fuku5022 <113103322+fuku5022@users.noreply.github.com>
2026-07-11 14:01:42 +0900

    Update LinkLayer.jsx

--- src/LinkLayer.jsx
+++ src/LinkLayer.jsx
@@ -8,8 +8,8 @@ export default function LinkLayer({ notes, links }) {
         position: "absolute",
         top: 0,
         left: 0,
-        width: "100%",
-        height: "100%",
+        width: 3000,
+        height: 2000,
         pointerEvents: "none",
       }}
     >
