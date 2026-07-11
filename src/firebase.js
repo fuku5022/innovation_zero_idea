81a8274b663964b4af80e0d2f7819f3cc927abc0
fuku5022 <113103322+fuku5022@users.noreply.github.com>
2026-07-11 13:33:58 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBLDNqORsdhEO3jO1IF-DmqFo7AA4gLwZg",
+  authDomain: "collab-sticky-board.firebaseapp.com",
+  databaseURL: "https://collab-sticky-board-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "collab-sticky-board",
+  storageBucket: "collab-sticky-board.firebasestorage.app",
+  messagingSenderId: "440971474452",
+  appId: "1:440971474452:web:822ce066158eb4f9b39901",
 };
 
 const app = initializeApp(firebaseConfig);
